ea5c42fbaf01833cf74b261ed0076976985963e0
András Leitereg <leanil94@gmail.com>
2016-04-05 22:48:32 +0300

    Make dll build configuration and platform sensitive.

--- FParLin/evaluator.cpp
+++ FParLin/evaluator.cpp
@@ -9,14 +9,14 @@
 
 using namespace std;
 
-function<vector<double>(map<string, vector<double>*>)> get_evaluator(Fix<F> tree, int threshold) {
+bool process_tree(Fix<F> tree, int threshold) {
 	typecheck_t checked = cata(typecheck_alg, tree);
 	if (!checked.second.empty()) {
 		cerr << "type mismatches were found:\n";
 		for (string error : checked.second) {
 			cerr << error << endl;
 		}
-		return function<vector<double>(map<string, vector<double>*>)>{};
+		return false;
 	}
 	checked.first = cata(costest_alg, checked.first);
 	ofstream result("result.cpp");
@@ -28,19 +28,53 @@ function<vector<double>(map<string, vector<double>*>)> get_evaluator(Fix<F> tree
 	result << "\t\treturn vectorize(" << cata(codegen_alg(threshold), checked.first).first << ");\n";
 	result << "\t}\n}\n";
 	result.close();
+	return true;
+}
 
+bool build_dll() {
 	ifstream config("config.txt");
-	string build_tool;
+	string build_tool, vcvars = "";
 	getline(config, build_tool);
-	system((build_tool + " /LD /MDd -fmsc-version=1900 -Wno-return-type-c-linkage result.cpp").c_str());
+	if (build_tool.find("cl.exe") != string::npos) {
+		getline(config, vcvars);
+	}
+	string common = "/LD /analyze- /GS- /W3 /fp:precise /Gd /Oy /EHsc /D \"_CONSOLE\" /D \"_LIB\" /D \"_UNICODE\" /D \"UNICODE\""
+		" -fmsc-version=1900 -Wno-return-type-c-linkage";
+#ifdef NDEBUG
+	string configuration = "/GL /Gm- /Gy /Oi /Zi /Ox /MD /D \"NDEBUG\"";
+#else
+	string configuration = "/Gm /Gy- /Oi- /ZI /Od /RTC1 /MDd /D \"_DEBUG\"";
+#endif
+#if _WIN64 || __x86_64__
+	string platform = "-m64";
+	vcvars += vcvars.empty() ? "" : " x86_amd64";
+#else
+	string platform = "/D \"WIN32\" -m32";
+	vcvars += vcvars.empty() ? "" : " x86";
+#endif
+	vcvars += vcvars.empty() ? "" : " && ";
+	string build_command = "\"" + vcvars + build_tool + " " + common + " " + configuration + " " + platform + " result.cpp\"";
+	cout << "building with:\n" << build_command << endl;
+	return !system(build_command.c_str());
+}
+
+bool link_dll(FARPROC& evaluator) {
 	HINSTANCE dll = LoadLibrary("result.dll");
 	if (!dll) {
 		cerr << "could not load dll\n";
-		return function<vector<double>(map<string, vector<double>*>)>{};
+		return false;
 	}
-	auto evaluator = GetProcAddress(dll, "evaluator");
+	evaluator = GetProcAddress(dll, "evaluator");
 	if (!evaluator) {
 		cerr << "could not get function address\n";
+		return false;
+	}
+	return true;
+}
+
+function<vector<double>(map<string, vector<double>*>)> get_evaluator(Fix<F> tree, int threshold) {
+	FARPROC evaluator;
+	if (!process_tree(tree, threshold) || !build_dll() || !link_dll(evaluator)) {
 		return function<vector<double>(map<string, vector<double>*>)>{};
 	}
 	return ((vector<double>(*)(map<string, vector<double>*>))evaluator);
